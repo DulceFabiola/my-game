60bdc8f4f309409b82fae0855d18abd36c148584
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-11-12 10:07:59 -0600

    cambio de monstruo

--- canvas.js
+++ canvas.js
@@ -17,6 +17,7 @@ const boardImage = "https://opengameart.org/sites/default/files/Brick_03.png";
 const gameOverImage = "/images/game-over.jpg";
 const youWinImage = "/images/you-win.jpg";
 const monsterImage = "/images/mike.png";
+const sullyImage = "/images/sully.png";
 const kidImage = "/images/boo.png";
 const monsterIcon = "/images/monster-coin.png";
 const energyIcon = "/images/energy-icon.png";
@@ -232,7 +233,7 @@ function drawKids() {
   kids.forEach((kid) => kid.draw());
 }
 function generateMonster(x, y) {
-  let monsterCanvas = new Character(x, y, 100, 100, monsterImage);
+  let monsterCanvas = new Character(x, y, 150, 150, sullyImage);
   monsters.push(monsterCanvas);
   generateScare(x, y);
   points -= 20;
